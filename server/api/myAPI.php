e2822d3654db8755db0e907d45cbd2e2d7e4c78f
Meir Sabbah <47792657+promermedia@users.noreply.github.com>
2019-04-09 11:54:25 +0300

    Delete myAPI.php

--- server/api/myAPI.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $fName = $_GET['fName'];
-    $lName = $_GET['lName'];
-    echo "<h1>My first name is $fName and my last name is $lName </h1>";
-}
-;
